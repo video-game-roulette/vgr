c83777289cc322b35999603c6a380397e0b60378
Andrew Joy <70282366+ajoy267@users.noreply.github.com>
2022-02-28 16:34:39 -0800

    Dev andrew (#4)
    * completed file structure
    
    * had to add the imports
    
    * add a test
    
    * added Home page content
    
    * added usercontext, client.js, user.js functions, and started login form
    
    * working on auth

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,20 +1,6 @@
 import React, { useState } from 'react';
-import { useUser } from '../../context/UserContext';
-import { useHistory, useLocation } from 'react-router-dom';
 
 export default function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const { setUser } = useUser();
-  const history = useHistory();
-  const location = useLocation();
-
-  const { from } = location.state || { from: { pathname: '/profile' } };
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-  };
 
   return (
     <div>
