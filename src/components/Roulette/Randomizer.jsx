ca2b9a8922848872eacd1937b7ce84042badd774
Brett Seifried <seifriedbrett@gmail.com>
2022-02-28 16:39:02 -0800

    component Roulette framework

--- src/components/Roulette/Randomizer.jsx
+++ src/components/Roulette/Randomizer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Randomizer() {
   return (
-    <div>
+    <div className="randomizer">
       <h1>You Shoulda Play:</h1>
       <h2>gametitle</h2>
       <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/46/River_City_Ransom-front.jpg/220px-River_City_Ransom-front.jpg" />
--- src/views/Roulette/Roulette.jsx
+++ src/views/Roulette/Roulette.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Randomizer from '../../components/Roulette/Randomizer';
+import './Roulette.css';
+
+// const { user }
+// const { game }
+// const [loading, setLoading] = useState(false);
 
 export default function Roulette() {
   return (
     <>
+      <Link to="/profile">
+        <button className="back-btn">Back</button>
+      </Link>
       <Randomizer />
     </>
   );
