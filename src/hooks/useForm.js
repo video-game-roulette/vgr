d20256088c615bc9377a7524b3c9694b26d2bc41
ajoy267 <andrew.joy12@hotmail.com>
2022-02-28 16:44:32 -0800

    resolved conflict

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,9 +1,5 @@
 import { useState } from 'react';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> c71bdc997ac83b914e536c1f914eeba4935af42e
 export default function useForm(inputs = {}) {
   const [formState, setFormState] = useState(inputs);
   const [formError, setFormError] = useState('');
