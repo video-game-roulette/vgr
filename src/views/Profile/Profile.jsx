f4ae201ba4579cc04d6cfe0e967fc3701bd49432
ajoy267 <andrew.joy12@hotmail.com>
2022-03-01 10:40:52 -0800

    made profile page skeleton

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import UserProfile from '../../components/Profile/UserProfile';
 
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
 }
