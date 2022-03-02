9806dfb37de4d0cc3c02b6f5f66e3476915ca614
Brett Seifried <88357710+BrettSeifried@users.noreply.github.com>
2022-03-02 12:24:05 -0800

    Brett2 (#29)
    * dont merge, testing
    
    * aboutUs page done
    
    * styled AboutUs
    
    * roulette page
    
    * component Roulette framework
    
    * Dev (#13)
    
    * made dev branch
    
    * Dev andrew (#1)
    
    * completed file structure
    
    * had to add the imports
    
    * add a test
    
    * created Readme file roughdraft (#2)
    
    * Dev andrew (#3)
    
    * completed file structure
    
    * had to add the imports
    
    * add a test
    
    * added Home page content
    
    * added usercontext, client.js, user.js functions, and started login form
    
    * Dev andrew (#4)
    
    * completed file structure
    
    * had to add the imports
    
    * add a test
    
    * added Home page content
    
    * added usercontext, client.js, user.js functions, and started login form
    
    * working on auth
    
    * login form, header (#5)
    
    * Brett2 (#6)
    
    * dont merge, testing
    
    * aboutUs page done
    
    * styled AboutUs
    
    * roulette page
    
    * component Roulette framework
    
    * Dev andrew (#7)
    
    * completed file structure
    
    * had to add the imports
    
    * add a test
    
    * added Home page content
    
    * added usercontext, client.js, user.js functions, and started login form
    
    * working on auth
    
    * resolved conflict
    
    * fixed broken app
    
    * login form, header (#8)
    
    * login form, header (#9)
    
    * login form, header (#10)
    
    * login form, header (#11)
    
    * Revert "login form, header (#8)" (#12)
    
    This reverts commit a0f46fcf1a45d4d33e1f65d5783feb933d86bbce.
    
    Co-authored-by: ajoy267 <andrew.joy12@hotmail.com>
    Co-authored-by: Andrew Joy <70282366+ajoy267@users.noreply.github.com>
    Co-authored-by: Brett Seifried <88357710+BrettSeifried@users.noreply.github.com>
    
    * working game services pulling from SupaBase
    
    * game Context created
    
    * AddEdit built rough cut
    
    * add game fleshed out
    
    * gameForm updated
    
    * adding game works
    
    * protected routes work
    
    * fixed add game info
    
    * working on edit
    
    Co-authored-by: Chase Crowder <gcrowder93@gmail.com>
    Co-authored-by: ajoy267 <andrew.joy12@hotmail.com>
    Co-authored-by: Andrew Joy <70282366+ajoy267@users.noreply.github.com>

--- src/components/Profile/UserProfile.jsx
+++ src/components/Profile/UserProfile.jsx
@@ -15,8 +15,13 @@ export default function UserProfile() {
       <Link to="/library">
         <button className="border-2 border-sky-500">Game Library</button>
       </Link>
+      {/* Move to game Library */}
       <Link to="/library/addgame">
         <button className="border-2 border-sky-500">Add Game</button>
+      </Link>
+      {/* Delete after Edit works */}
+      <Link to="/library/edit/9">
+        <button className="border-2 border-sky-500">edit</button>
       </Link>
     </div>
   );
--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useUser } from '../../context/UserContext';
+import { Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
-export default function ProtectedRoute() {
-  return <div>ProtectedRoute</div>;
+export default function ProtectedRoute({ children, ...routeProps }) {
+  const { user } = useUser();
+
+  return (
+    <>
+      <Route
+        {...routeProps}
+        render={({ location }) =>
+          user ? (
+            children
+          ) : (
+            <Redirect to={{ pathname: '/login', state: { from: location } }} />
+          )
+        }
+      />
+    </>
+  );
 }
