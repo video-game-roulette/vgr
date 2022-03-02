eb0d741f560492831c4de4a11d7fdd67abb7a8c6
ajoy267 <andrew.joy12@hotmail.com>
2022-03-02 14:41:21 -0800

    got my test to pass

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import { GameProvider } from './context/GameContext';
+import { UserProvider } from './context/UserContext';
 
 test('testing app', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <UserProvider>
+        <GameProvider>
+          <App />
+        </GameProvider>
+      </UserProvider>
+    </MemoryRouter>
+  );
   const heading = screen.getByRole('heading', { name: /video game roulette/i });
   expect(heading).toBeInTheDocument();
 });
--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
 import UserProfile from '../../components/Profile/UserProfile';
 import { useGame } from '../../context/GameContext';
 import { fetchGames } from '../../services/game';
@@ -8,14 +6,12 @@ import { fetchGames } from '../../services/game';
 export default function Profile() {
   const { game, setGame } = useGame();
   const [loading, setLoading] = useState(true);
-  console.log('games', game);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchGames();
       setGame(data);
       setLoading(false);
-      console.log('data', data);
     };
     fetchData();
   }, []);
