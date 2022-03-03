d0b6c3e5769d816ffb106b43bf6d7bc369ad3fae
Mark Voltaire <markjvoltaire@gmail.com>
2022-03-03 17:07:51 -0500

    images show in a row now

--- src/components/Profile/UserProfile.jsx
+++ src/components/Profile/UserProfile.jsx
@@ -9,19 +9,32 @@ export default function UserProfile({ game }) {
 
   return (
     <div>
-      <h1>Welcome {user}</h1>
+      <h1 className="font-extrabold text-slate-100 flex justify-center">
+        Welcome {user}
+      </h1>
       <Link to={`/library/9`}>
-        <button className="border-2 border-sky-500">Find A Game</button>
+        <button className="bg-slate-100 rounded-md font-bold text-blue-600 px-1 ">
+          Find A Game
+        </button>
       </Link>
 
       <Link to="/library/addgame">
-        <button className="border-2 border-sky-500">Add Game</button>
+        <button className="bg-indigo-500 rounded-md font-bold text-slate-100 px-1 left-2">
+          Add Game
+        </button>
       </Link>
-      <div>
+      <div className="flex flex-wrap">
         {data.map((item) => (
-          <div key={item.id}>
+          <div
+            className=" pt-3 flex flex-col w-1/2 sm:w-1/3 mt-5 "
+            key={item.id}
+          >
             <Link to={`/profile/${item.id}`}>
-              <h1>{item.title}</h1>
+              <img
+                className="object-cover h-28 w-28 rounded-md flex columns-2 mt-5"
+                src={item.image}
+              />
+              <h1 className="font-bold px-2 text-grey-700">{item.title}</h1>
             </Link>
           </div>
         ))}
