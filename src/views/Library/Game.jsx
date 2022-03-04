a3ef5a9c9481267320569668fa024360c6493ceb
Brett Seifried <88357710+BrettSeifried@users.noreply.github.com>
2022-03-03 18:09:16 -0800

    cleared merge conflicts

--- src/views/Library/Game.jsx
+++ src/views/Library/Game.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 import GameDetails from '../../components/Library/GameDetails';
 
 export default function Game() {
-<<<<<<< HEAD
-
-=======
->>>>>>> 40210620dc09d0f835e9eaaeaecc4d13f744e353
   return (
     <div>
       <GameDetails />
     </div>
   );
-<<<<<<< HEAD
-
-=======
->>>>>>> 40210620dc09d0f835e9eaaeaecc4d13f744e353
 }
